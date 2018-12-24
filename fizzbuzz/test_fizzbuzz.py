0e19e57d1e7e0a6008af0a0c3a22d97b89d04ee4
JF Marronnier <jfmarronnier@epitez.fr>
2018-12-24 17:29:50 +0100

    [ROUGE] Ajoute test Buzz pour multiple de 10

--- fizzbuzz/test_fizzbuzz.py
+++ fizzbuzz/test_fizzbuzz.py
@@ -6,3 +6,7 @@ class TestFizzBuzz(TestCase):
     def test_first(self):
         my_fizzbuzz = FizzBuzz
         self.assertEqual("1", my_fizzbuzz.say(1))
+
+    def test_buzz_10(self):
+        my_fizzbuzz = FizzBuzz
+        self.assertEqual("Buzz", my_fizzbuzz.say(10))
