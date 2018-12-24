d28d3c47bf041a6a3fb0e6830b9232fff9ead386
JF Marronnier <jfmarronnier@epitez.fr>
2018-12-24 17:04:36 +0100

    [ROUGE] Ajoute tests "3 --> Fizz"

--- fizzbuzz/test_fizzbuzz.py
+++ fizzbuzz/test_fizzbuzz.py
@@ -6,3 +6,7 @@ class TestFizzBuzz(TestCase):
     def test_first(self):
         my_fizzbuzz = FizzBuzz
         self.assertEqual("1", my_fizzbuzz.say(1))
+
+    def test_traite_fizz(self):
+        my_fizzbuzz = FizzBuzz
+        self.assertEqual("Fizz", my_fizzbuzz.say(3))
